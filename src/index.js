6d921627e081673e90289a812646aa911af7cd64
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-23 15:43:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Pb7";
+const number = 7;
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Your lucky number is {number}</p>
+  </div>,
+  document.getElementById("root")
+);
