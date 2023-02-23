b411f8e2435aac77cea04eee707bef22a93399e6
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-23 15:54:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+/* index.js hooche js file
+jetar bhetore amra html likhte parchi using jsx
+abar  html tar bhetorei amra js likhte  parchi using curly brackets
+e.g.    <p>Your lucky number is {number}</p>
+but amra normal js code likhte parbona like
+if(...){}
+  else {...}
+as eiguno statement
+amra expression use korte parbo curly braces{} r bhetore
+but statement or code jeta computer ke khatacche ota use korte parbona 
+as if statement computer ke chech korte bolche i.e. lhs === rhs kina
+so computer khatche so we cant use statements in jsx file
+*/
